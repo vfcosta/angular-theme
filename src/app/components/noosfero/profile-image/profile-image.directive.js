08013629f6ea3c38fa440d126f97d42fbddbbb2b
Victor Costa <vfcosta@gmail.com>
2016-01-14 11:28:06 -0300

    Create directive for profile image

---
+++ src/app/components/noosfero/profile-image/profile-image.directive.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angular')
+    .directive('noosferoProfileImage', noosferoProfileImage);
+
+  /** @ngInject */
+  function noosferoProfileImage() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/noosfero/profile-image/profile-image.html',
+      scope: {
+        profile: '='
+      },
+      controller: ProfileImageController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function ProfileImageController() {
+      var vm = this;
+      vm.defaultIcon = 'fa-users';
+      if(vm.profile.type==='Person') vm.defaultIcon = 'fa-user';
+    }
+  }
+
+})();
