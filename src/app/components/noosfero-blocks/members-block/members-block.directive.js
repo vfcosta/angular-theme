d3af38a5f2a595aa9ad2b64c25898c09422f4a9b
Victor Costa <vfcosta@gmail.com>
2016-01-14 14:04:01 -0300

    Add members block

---
+++ src/app/components/noosfero-blocks/members-block/members-block.directive.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angular')
+    .directive('noosferoMembersBlock', noosferoMembersBlock);
+
+  /** @ngInject */
+  function noosferoMembersBlock() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/noosfero-blocks/members-block/members-block.html',
+      scope: {
+          block: '=',
+          owner: '='
+      },
+      controller: MembersBlockController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function MembersBlockController(noosfero) {
+      var vm = this;
+      vm.members = [];
+
+      noosfero.members(noosfero.profiles.one(vm.owner.id)).one().get().then(function(result) {
+        vm.members = result.people;
+      });
+    }
+  }
+
+})();
--- src/app/components/noosfero/noosfero.service.js
+++ src/app/components/noosfero/noosfero.service.js
@@ -4,6 +4,9 @@
   angular.module('angular').factory('noosfero', function(Restangular) {
     return {
       profiles: Restangular.service('profiles'),
+      members: function(profile) {
+        return Restangular.service('members', profile);
+      },
       boxes: function(profileId) {
         return Restangular.service('boxes', Restangular.one('profiles', profileId))
       }
