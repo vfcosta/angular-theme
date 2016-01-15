052d904ea48f87d1670b8e68c89cc157454c06f2
Victor Costa <vfcosta@gmail.com>
2016-01-15 17:01:08 -0300

    Keep url when redirect to profile home page

--- src/app/profile/profile-home.controller.js
+++ src/app/profile/profile-home.controller.js
@@ -15,9 +15,9 @@
       vm.profile = $scope.vm.owner;
       noosfero.profile(vm.profile.id).customGET('home_page', {fields: 'path'}).then(function(result) {
         if(result.article) {
-          $state.transitionTo('main.profile.page', {page: result.article.path, profile: vm.profile.identifier});
+          $state.transitionTo('main.profile.page', {page: result.article.path, profile: vm.profile.identifier}, {location: false});
         } else {
-          $state.transitionTo('main.profile.info', {profile: vm.profile.identifier});
+          $state.transitionTo('main.profile.info', {profile: vm.profile.identifier}, {location: false});
         }
       });
     }
