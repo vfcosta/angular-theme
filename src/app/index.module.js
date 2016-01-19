aa8711cd270fc04a23dbd8fb42e6eea48db27ce8
Victor Costa <vfcosta@gmail.com>
2016-01-19 10:23:06 -0300

    Add recent documents block

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,6 +5,6 @@
     .module('angular', ['ngAnimate', 'ngCookies', 'ngStorage', 'ngTouch',
                         'ngSanitize', 'ngMessages', 'ngAria', 'restangular',
                         'ui.router', 'ui.bootstrap', 'toastr',
-                        'angularMoment', 'angular.filter']);
+                        'angularMoment', 'angular.filter', 'akoenig.deckgrid']);
 
 })();
