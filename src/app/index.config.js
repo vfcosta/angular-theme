335af8a0435a5696a47612969a5f64ef5fbc39b1
Victor Costa <vfcosta@gmail.com>
2016-01-19 17:04:09 -0300

    Smoth scrolling

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,11 +6,17 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, $locationProvider, RestangularProvider, $httpProvider) {
+  function config($logProvider, $locationProvider, RestangularProvider, $httpProvider, $provide) {
     $logProvider.debugEnabled(true);
     $locationProvider.html5Mode({enabled: true});
     RestangularProvider.setBaseUrl('/api/v1');
     $httpProvider.defaults.headers.post = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'};
+
+    $provide.decorator('$uiViewScroll', function ($delegate, $document) {
+      return function (uiViewElement) {
+        $document.scrollToElementAnimated(uiViewElement);
+      };
+    });
   }
 
 })();
