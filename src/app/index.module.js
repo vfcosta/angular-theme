016d2889b590389f62d4cdcb1b34ca735297e4b1
Victor Costa <vfcosta@gmail.com>
2016-01-19 13:25:43 -0300

    Use angular timeline module

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,6 +5,7 @@
     .module('angular', ['ngAnimate', 'ngCookies', 'ngStorage', 'ngTouch',
                         'ngSanitize', 'ngMessages', 'ngAria', 'restangular',
                         'ui.router', 'ui.bootstrap', 'toastr',
-                        'angularMoment', 'angular.filter', 'akoenig.deckgrid']);
+                        'angularMoment', 'angular.filter', 'akoenig.deckgrid',
+                        'angular-timeline']);
 
 })();
