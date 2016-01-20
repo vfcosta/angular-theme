1b84784dec47b98b0d2f29836f5387f3213f4d4b
Victor Costa <vfcosta@gmail.com>
2016-01-20 16:03:10 -0300

    Disable proxy for myprofile in dev environment

--- gulp/server.js
+++ gulp/server.js
@@ -27,12 +27,11 @@ function browserSyncInit(baseDir, browser) {
   };
 
   server.middleware = [proxyMiddleware('http://localhost:3000/api', {changeOrigin:true}),
-                       proxyMiddleware('http://localhost:3000/myprofile', {changeOrigin:true}),
+                      //  proxyMiddleware('http://localhost:3000/myprofile', {changeOrigin:true}),
                        proxyMiddleware('http://localhost:3000/designs', {changeOrigin:true}),
                        proxyMiddleware('http://localhost:3000/image_uploads', {changeOrigin:true}),
                        proxyMiddleware('http://localhost:3000/account/logout', {changeOrigin:true}),
                        proxyMiddleware('http://localhost:3000/images', {changeOrigin:true})];
-                      //  proxyMiddleware('http://localhost:3000/assets', {changeOrigin:true})];
 
   browserSync.instance = browserSync.init({
     startPath: '/',
