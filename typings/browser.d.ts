4946a477fd523398f2a1cc37feedcf1be1c27be5
ABNER SILVA DE OLIVEIRA <abner.oliveira@serpro.gov.br>
2016-02-25 19:11:45 -0300

    fix previous commit

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="browser/ambient/angular/angular.d.ts" />
-/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="browser/ambient/jquery/jquery.d.ts" />
-/// <reference path="browser/ambient/restangular/restangular.d.ts" />
--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="main/ambient/angular/angular.d.ts" />
-/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="main/ambient/jquery/jquery.d.ts" />
-/// <reference path="main/ambient/restangular/restangular.d.ts" />
