ef326a93c92b9246df0d002ddec8d6aa9bea8e9c
Victor Costa <vfcosta@gmail.com>
2016-03-01 18:28:06 -0300

    Refactor article component

--- src/app/components/noosfero-articles/article/article.directive.ts
+++ src/app/components/noosfero-articles/article/article.directive.ts
@@ -2,38 +2,22 @@ import { bundle, Input, Inject, Component, Directive } from 'ng-forward';
 import {NoosferoArticleBlog} from "../blog/blog.component";
 
 @Component({
-    selector: 'noosfero-default-article',
-    templateUrl: 'app/components/noosfero-articles/article/article.html'
-})
-export class ArticleView {
-
-    @Input() article: any;
-    @Input() profile: any;
-
-    constructor() {
-
-    }
-}
-
-@Component({
     selector: 'noosfero-article',
-    template: '<div></div>',
-    directives: [ArticleView, NoosferoArticleBlog]
+    templateUrl: 'app/components/noosfero-articles/article/article.html',
+    directives: [NoosferoArticleBlog]
 })
 @Inject("$element", "$scope", "$injector", "$compile")
 export class ArticleDirective {
 
     @Input() article: any;
     @Input() profile: any;
-    directiveName: string;
 
     ngOnInit() {
         let specificDirective = 'noosfero' + this.article.type;
-        this.directiveName = "noosfero-default-article";
         if (this.$injector.has(specificDirective + 'Directive')) {
-            this.directiveName = specificDirective.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+            let directiveName = specificDirective.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+            this.$element.replaceWith(this.$compile('<' + directiveName + ' [article]="ctrl.article" [profile]="ctrl.profile"></' + directiveName + '>')(this.$scope));
         }
-        this.$element.replaceWith(this.$compile('<' + this.directiveName + ' [article]="ctrl.article" [profile]="ctrl.profile"></' + this.directiveName + '>')(this.$scope));
     }
 
     constructor(private $element: any, private $scope: ng.IScope, private $injector: ng.auto.IInjectorService, private $compile: ng.ICompileService) {
