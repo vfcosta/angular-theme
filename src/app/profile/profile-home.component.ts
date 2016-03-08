5368292db8eb99227dcd5088b46e94160a37acd9
Victor Costa <vfcosta@gmail.com>
2016-03-08 16:36:03 -0300

    Add tests to profile home component

--- src/app/profile/profile-home.component.ts
+++ src/app/profile/profile-home.component.ts
@@ -8,15 +8,15 @@ import {ProfileService} from "../../lib/ng-noosfero-api/http/profile.service";
     template: "<div></div>",
     providers: [provide('profileService', { useClass: ProfileService })]
 })
-@Inject(ProfileService, "$log", "$stateParams", "$scope", "$state")
+@Inject(ProfileService, "$state")
 export class ProfileHome {
 
     profile: Profile;
 
-    constructor(profileService: ProfileService, $log: ng.ILogService, $stateParams: ng.ui.IStateParamsService, $scope: ng.IScope, $state: ng.ui.IStateService) {
+    constructor(profileService: ProfileService, $state: ng.ui.IStateService) {
         profileService.getCurrentProfile().then((profile: Profile) => {
             this.profile = profile;
-            return profileService.get(this.profile.id).customGET('home_page', { fields: 'path' });
+            return profileService.getHomePage(this.profile.id, { fields: 'path' });
         }).then((response: restangular.IResponse) => {
             if (response.data.article) {
                 $state.transitionTo('main.profile.page', { page: response.data.article.path, profile: this.profile.identifier }, { location: false });
