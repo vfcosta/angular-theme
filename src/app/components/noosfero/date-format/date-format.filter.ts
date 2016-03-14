a2e78a1a616b6157d700b742d743c63f3a3d1ab3
Victor Costa <vfcosta@gmail.com>
2016-03-14 10:51:33 -0300

    Use angular moment filter to parse dates

--- src/app/components/noosfero/date-format/date-format.filter.ts
+++ src/app/components/noosfero/date-format/date-format.filter.ts
@@ -1,10 +1,13 @@
 import {Pipe, Inject} from "ng-forward";
 
 @Pipe("dateFormat")
+@Inject("amParseFilter")
 export class DateFormat {
 
+    constructor(private amParseFilter: any) { }
+
     transform(date: string, options: any) {
-        return moment(date, "YYYY/MM/DD HH:mm:ss");
+        return this.amParseFilter(date, "YYYY/MM/DD HH:mm:ss").toISOString();
     }
 
 }
