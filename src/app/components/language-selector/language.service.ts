f082feed3feb02387c2eea02f0c839653ad4270c
Victor Costa <vfcosta@gmail.com>
2016-03-15 18:43:21 -0300

    Refactor language component

---
+++ src/app/components/language-selector/language.service.ts
@@ -0,0 +1,59 @@
+import {Injectable, Inject} from "ng-forward";
+
+@Injectable()
+@Inject("$translate", "tmhDynamicLocale", "amMoment", "angularLoad", "$rootScope")
+export class LanguageService {
+
+    availableLanguages: any;
+
+    constructor(private $translate: angular.translate.ITranslateService,
+        private tmhDynamicLocale: angular.dynamicLocale.tmhDynamicLocaleService,
+        private amMoment: any,
+        private angularLoad: any,
+        private $rootScope: any) {
+
+        this.configAvailableLanguages();
+        this.$rootScope.$on("$localeChangeSuccess", () => {
+            this.changeLanguage(tmhDynamicLocale.get() || $translate.use());
+        });
+    }
+
+    currentLanguage() {
+        return this.$translate.use();
+    }
+
+    changeLanguage(language: string) {
+        if (!language) {
+            console.log("WARN: language undefined");
+            return;
+        }
+        this.changeMomentLocale(language);
+        this.tmhDynamicLocale.set(language);
+        this.angularLoad.loadScript(`/bower_components/messageformat/locale/${language}.js`).then(() => {
+            return this.$translate.use(language);
+        }).then(() => {
+            this.configAvailableLanguages();
+        });
+    }
+
+    translate(text: string) {
+        return this.$translate.instant(text);
+    }
+
+    private configAvailableLanguages() {
+        this.availableLanguages = {
+            "en": this.$translate.instant("language.en"),
+            "pt": this.$translate.instant("language.pt")
+        };
+    }
+
+    private changeMomentLocale(language: string) {
+        let localePromise = Promise.resolve();
+        if (language != "en") {
+            localePromise = this.angularLoad.loadScript(`/bower_components/moment/locale/${language}.js`);
+        }
+        localePromise.then(() => {
+            this.amMoment.changeLocale(language);
+        });
+    }
+}
--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -1,13 +1,14 @@
 import {Injectable, Inject} from "ng-forward";
+import {LanguageService} from "../language-selector/language.service";
 
 @Injectable()
-@Inject("$log", "SweetAlert", "$translate")
+@Inject("$log", "SweetAlert", LanguageService)
 export class Notification {
 
     constructor(
         private $log: ng.ILogService,
         private SweetAlert: any,
-        private $translate: angular.translate.ITranslateService
+        private languageService: LanguageService
     ) { }
 
     public static DEFAULT_ERROR_TITLE = "notification.error.default.title";
@@ -15,16 +16,15 @@ export class Notification {
     public static DEFAULT_SUCCESS_TIMER = 1000;
 
     error(message: string = Notification.DEFAULT_ERROR_MESSAGE, title: string = Notification.DEFAULT_ERROR_TITLE) {
-        this.$log.debug("Notification error:", title, message);
+        this.$log.debug("Notification error:", title, message, this.languageService.currentLanguage());
         this.SweetAlert.swal({
-            title: this.$translate.instant(title),
-            text: this.$translate.instant(message),
+            title: this.languageService.translate(title),
+            text: this.languageService.translate(message),
             type: "error"
         });
     }
 
     httpError(status: number, data: any): boolean {
-        let message = (data || {}).message || Notification.DEFAULT_ERROR_MESSAGE;
         this.error(`notification.http_error.${status}.message`);
         return true; // return true to indicate that the error was already handled
     }
