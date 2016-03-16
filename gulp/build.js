c7eb46d45a8619ec5068dca4a9a0b68ec1d16dbb
Carlos Purificacao <carloseugenio@gmail.com>
2016-03-16 09:32:08 -0300

    Added ngdocs configuration

--- gulp/build.js
+++ gulp/build.js
@@ -98,4 +98,8 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
+gulp.task('clean-docs', [], function() {
+    return $.del([path.join(conf.paths.docs, '/')]);    
+});
+
 gulp.task('build', ['html', 'fonts', 'other']);
