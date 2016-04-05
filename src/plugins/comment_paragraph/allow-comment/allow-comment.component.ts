31b34a75ce85c36bba7f192e58614e8f4e328437
Victor Costa <vfcosta@gmail.com>
2016-04-05 17:29:44 -0300

    Display paragraph comments besides the article

--- src/plugins/comment_paragraph/allow-comment/allow-comment.component.ts
+++ src/plugins/comment_paragraph/allow-comment/allow-comment.component.ts
@@ -1,16 +1,14 @@
 import {Component, Input} from "ng-forward";
+import {SideCommentsComponent} from "../side-comments/side-comments.component";
 
 @Component({
     selector: "comment-paragraph-plugin-allow-comment",
-    templateUrl: "plugins/comment_paragraph/allow-comment/allow-comment.html"
+    templateUrl: "plugins/comment_paragraph/allow-comment/allow-comment.html",
+    directives: [SideCommentsComponent]
 })
-export class AllowComment {
+export class AllowCommentComponent {
 
     @Input() content: string;
     @Input() paragraphUuid: string;
     @Input() article: noosfero.Article;
-
-    constructor() {
-        console.log("ALLOW COMMENT");
-    }
 }
