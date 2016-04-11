abdfbb354e4604568eb87ab57c61dcee9a8525ab
Victor Costa <vfcosta@gmail.com>
2016-04-11 12:10:08 -0300

    Activate/deactivate comment paragraph in articles

--- src/plugins/comment_paragraph/hotspot/comment-paragraph-article-button.component.ts
+++ src/plugins/comment_paragraph/hotspot/comment-paragraph-article-button.component.ts
@@ -1,15 +1,28 @@
 import { Input, Inject, Component } from "ng-forward";
 import {Hotspot} from "../../../app/hotspot/hotspot.decorator";
+import {CommentParagraphService} from "../http/comment-paragraph.service";
 
 @Component({
     selector: "comment-paragraph-article-button-hotspot",
-    templateUrl: "plugins/comment_paragraph/hotspot/article-button.html",
+    templateUrl: "plugins/comment_paragraph/hotspot/comment-paragraph-article-button.html",
 })
-@Inject("$scope")
+@Inject("$scope", CommentParagraphService)
 @Hotspot("article_extra_toolbar_buttons")
 export class CommentParagraphArticleButtonHotspotComponent {
 
     @Input() article: noosfero.Article;
 
-    constructor(private $scope: ng.IScope) { }
+    constructor(private $scope: ng.IScope, private commentParagraphService: CommentParagraphService) { }
+
+    deactivateCommentParagraph() {
+        this.commentParagraphService.deactivateCommentParagraph(this.article).then((result: noosfero.RestResult<noosfero.Article>) => {
+            this.article = result.data;
+        });
+    }
+
+    activateCommentParagraph() {
+        this.commentParagraphService.activateCommentParagraph(this.article).then((result: noosfero.RestResult<noosfero.Article>) => {
+            this.article = result.data;
+        });
+    }
 }
