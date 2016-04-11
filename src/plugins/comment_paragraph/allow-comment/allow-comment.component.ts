8a1cb95c58080d9367fb2a5905abbcc5f7b284d9
Victor Costa <vfcosta@gmail.com>
2016-04-11 14:12:20 -0300

    Display comment counts beside paragraphs

--- src/plugins/comment_paragraph/allow-comment/allow-comment.component.ts
+++ src/plugins/comment_paragraph/allow-comment/allow-comment.component.ts
@@ -1,25 +1,32 @@
 import {Component, Input, Inject} from "ng-forward";
 import {SideCommentsComponent} from "../side-comments/side-comments.component";
 import {CommentParagraphEventService} from "../events/comment-paragraph-event.service";
+import {CommentParagraphService} from "../http/comment-paragraph.service";
 
 @Component({
     selector: "comment-paragraph-plugin-allow-comment",
     templateUrl: "plugins/comment_paragraph/allow-comment/allow-comment.html",
     directives: [SideCommentsComponent]
 })
-@Inject("$scope", CommentParagraphEventService)
+@Inject("$scope", CommentParagraphEventService, CommentParagraphService)
 export class AllowCommentComponent {
 
     @Input() content: string;
     @Input() paragraphUuid: string;
     @Input() article: noosfero.Article;
+    commentsCount: number;
 
-    constructor(private $scope: ng.IScope, private commentParagraphEventService: CommentParagraphEventService) { }
+    constructor(private $scope: ng.IScope,
+        private commentParagraphEventService: CommentParagraphEventService,
+        private commentParagraphService: CommentParagraphService) { }
 
     ngOnInit() {
         this.commentParagraphEventService.subscribeToggleCommentParagraph((article: noosfero.Article) => {
             this.article = article;
             this.$scope.$apply();
+        });
+        this.commentParagraphService.commentParagraphCount(this.article, this.paragraphUuid).then((count: any) => {
+            this.commentsCount = count;
         });
     }
 
