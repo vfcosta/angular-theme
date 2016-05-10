c046c510dd0df43719d26e55b36d076c2bfee021
Victor Costa <vfcosta@gmail.com>
2016-05-10 16:42:27 -0300

    Display period for discussions in comment paragraph plugin

---
+++ src/plugins/comment_paragraph/hotspot/article-content/article-content.component.ts
@@ -0,0 +1,12 @@
+import { Input, Inject, Component } from "ng-forward";
+import {Hotspot} from "../../../../app/hotspot/hotspot.decorator";
+
+@Component({
+    selector: "comment-paragraph-article-content-hotspot",
+    templateUrl: "plugins/comment_paragraph/hotspot/article-content/article-content.html",
+})
+@Hotspot("article_extra_content")
+export class CommentParagraphArticleContentHotspotComponent {
+
+    @Input() article: noosfero.Article;
+}
