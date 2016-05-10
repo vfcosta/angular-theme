9cb82c9853b8e9ec2e522170165a89333534b774
Leandro Nunes dos Santos <leandronunes@gmail.com>
2016-05-10 09:06:24 -0300

    removing warning

--- src/app/index.ts
+++ src/app/index.ts
@@ -15,7 +15,7 @@ declare var moment: any;
 let noosferoApp: any = bundle("noosferoApp", MainComponent, ["ngAnimate", "ngCookies", "ngStorage", "ngTouch",
     "ngSanitize", "ngMessages", "ngAria", "restangular",
     "ui.router", "ui.bootstrap", "toastr", "ngCkeditor",
-    "angular-bind-html-compile","angularMoment", "angular.filter", "akoenig.deckgrid",
+    "angular-bind-html-compile", "angularMoment", "angular.filter", "akoenig.deckgrid",
     "angular-timeline", "duScroll", "oitozero.ngSweetAlert",
     "pascalprecht.translate", "tmh.dynamicLocale", "angularLoad",
     "angular-click-outside"]).publish();
