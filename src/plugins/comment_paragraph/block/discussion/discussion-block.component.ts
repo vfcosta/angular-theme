0f9e6899064522e9c4fad7d63930a49326bf2e34
Victor Costa <vfcosta@gmail.com>
2016-05-16 10:26:41 -0300

    Use api_content to get articles for discussion block

--- src/plugins/comment_paragraph/block/discussion/discussion-block.component.ts
+++ src/plugins/comment_paragraph/block/discussion/discussion-block.component.ts
@@ -1,48 +1,26 @@
 import {Component, Inject, Input} from "ng-forward";
-import {ArticleService} from "../../../../lib/ng-noosfero-api/http/article.service";
+import {BlockService} from "../../../../lib/ng-noosfero-api/http/block.service";
 
 @Component({
     selector: "noosfero-comment-paragraph-plugin-discussion-block",
     templateUrl: 'plugins/comment_paragraph/block/discussion/discussion-block.html'
 })
-@Inject(ArticleService, "$state")
+@Inject(BlockService, "$state")
 export class DiscussionBlockComponent {
 
     @Input() block: any;
     @Input() owner: any;
 
-    profile: any;
-    documents: any;
+    profile: noosfero.Profile;
+    documents: Array<noosfero.Article>;
 
-    documentsLoaded: boolean = false;
-
-    constructor(private articleService: ArticleService, private $state: any) { }
+    constructor(private blockService: BlockService, private $state: any) { }
 
     ngOnInit() {
         this.profile = this.owner;
-        this.documents = [];
-
-        let limit = ((this.block && this.block.settings) ? this.block.settings.limit : null) || 50;
-        let params: any = { content_type: 'CommentParagraphPlugin::Discussion', per_page: limit, order: 'start_date DESC' };
-        let now = new Date().toISOString();
-        switch (this.block.settings['discussion_status']) {
-            case 0:
-                params['from_start_date'] = now;
-                break;
-            case 1:
-                params['until_start_date'] = now;
-                params['from_end_date'] = now;
-                break;
-            case 2:
-                params['until_end_date'] = now;
-                break;
-        }
-        console.log(this.block.settings['discussion_status']);
-        this.articleService.getByProfile(this.profile, params)
-            .then((result: noosfero.RestResult<noosfero.Article[]>) => {
-                this.documents = <noosfero.Article[]>result.data;
-                this.documentsLoaded = true;
-            });
+        this.blockService.getApiContent(this.block).then((content: any) => {
+            this.documents = content.articles;
+        });
     }
 
     openDocument(article: any) {
