4902da4db0e3b5fc5bfb9a5c492017b26f37fd21
Victor Costa <vfcosta@gmail.com>
2016-05-18 11:47:49 -0300

    Fix style paths in gulp watch

--- gulp/watch.js
+++ gulp/watch.js
@@ -15,11 +15,13 @@ gulp.task('watch', ['inject'], function () {
 
   gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
 
-  gulp.watch([
-    path.join(conf.paths.src, '/app/**/*.css'),
-    path.join(conf.paths.src, '/app/**/*.scss'),
-    path.join(conf.paths.src, conf.paths.plugins, '/**/*.scss')
-  ], function(event) {
+  var stylePaths = [path.join(conf.paths.src, conf.paths.plugins, '/**/*.scss')];
+  conf.paths.allSources.forEach(function(src) {
+    stylePaths.push(path.join(src, '/app/**/*.css'));
+    stylePaths.push(path.join(src, '/app/**/*.scss'));
+  });
+
+  gulp.watch(stylePaths, function(event) {
     if(isOnlyChange(event)) {
       gulp.start('styles-reload');
     } else {
