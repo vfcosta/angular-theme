0e907d92b145685166bfdfe2ebbfa32dd47f81e4
Victor Costa <vfcosta@gmail.com>
2016-05-20 09:07:51 -0300

    Remove article button from comment paragraph

--- src/plugins/comment_paragraph/index.ts
+++ src/plugins/comment_paragraph/index.ts
@@ -1,9 +1,8 @@
 import {AllowCommentComponent} from "./allow-comment/allow-comment.component";
-import {CommentParagraphArticleButtonHotspotComponent} from "./hotspot/comment-paragraph-article-button.component";
 import {CommentParagraphFormHotspotComponent} from "./hotspot/comment-paragraph-form.component";
 import {DiscussionEditorComponent} from "./article/cms/discussion-editor/discussion-editor.component";
 import {CommentParagraphArticleContentHotspotComponent} from "./hotspot/article-content/article-content.component";
 import {DiscussionBlockComponent} from "./block/discussion/discussion-block.component";
 
 export let mainComponents: any = [AllowCommentComponent, DiscussionEditorComponent, DiscussionBlockComponent];
-export let hotspots: any = [CommentParagraphArticleButtonHotspotComponent, CommentParagraphFormHotspotComponent, CommentParagraphArticleContentHotspotComponent];
+export let hotspots: any = [CommentParagraphFormHotspotComponent, CommentParagraphArticleContentHotspotComponent];
