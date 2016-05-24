4de49a37ba5215c9d4f0541eae6a5fe2e2b6fba5
Carlos Purificacao <carloseugenio@gmail.com>
2016-05-24 10:58:30 -0300

    Added removed listener to RecentDocuments Block

--- src/app/layout/blocks/recent-documents/recent-documents-block.component.ts
+++ src/app/layout/blocks/recent-documents/recent-documents-block.component.ts
@@ -1,11 +1,13 @@
 import {Component, Inject, Input} from "ng-forward";
 import {BlockService} from "../../../../lib/ng-noosfero-api/http/block.service";
+import {ArticleService} from "./../../../../lib/ng-noosfero-api/http/article.service"
+import {Arrays} from "./../../../../lib/util/arrays"
 
 @Component({
     selector: "noosfero-recent-documents-block",
     templateUrl: 'app/layout/blocks/recent-documents/recent-documents-block.html'
 })
-@Inject(BlockService, "$state")
+@Inject(BlockService, "$state", ArticleService)
 export class RecentDocumentsBlockComponent {
 
     @Input() block: any;
@@ -15,7 +17,7 @@ export class RecentDocumentsBlockComponent {
     documents: any;
     documentsLoaded: boolean = false;
 
-    constructor(private blockService: BlockService, private $state: any) { }
+    constructor(private blockService: BlockService, private $state: any, public articleService: ArticleService) { }
 
     ngOnInit() {
         this.profile = this.owner;
@@ -24,7 +26,14 @@ export class RecentDocumentsBlockComponent {
             this.documents = content.articles;
             this.documentsLoaded = true;
         });
+        this.watchArticles();
     }
+    
+    watchArticles() {
+        this.articleService.subscribeToArticleRemoved((article: noosfero.Article) => {
+            Arrays.remove(this.documents, article);
+        });
+    }    
 
     openDocument(article: any) {
         this.$state.go("main.profile.page", { page: article.path, profile: article.profile.identifier });
