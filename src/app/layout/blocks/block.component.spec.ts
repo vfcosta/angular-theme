62f85230b4028963680cebe137982f1b6e3a0caa
Victor Costa <vfcosta@gmail.com>
2016-06-14 10:40:59 -0300

    Move logic that show/hide blocks to block component

---
+++ src/app/layout/blocks/block.component.spec.ts
@@ -0,0 +1,135 @@
+import {Component} from 'ng-forward';
+import {BlockComponent} from './block.component';
+import * as helpers from "../../../spec/helpers";
+import {ComponentTestHelper, createClass} from '../../../spec/component-test-helper';
+
+const htmlTemplate: string = '<noosfero-block [block]="ctrl.block" [owner]="ctrl.profile"></noosfero-block>';
+
+describe("Boxes Component", () => {
+
+    let helper: ComponentTestHelper<BlockComponent>;
+    beforeEach(() => {
+        angular.mock.module("templates");
+    });
+
+    let properties = {
+        block: { id: 1 },
+        owner: {
+            id: 1,
+            identifier: 'profile-name',
+            type: 'Person'
+        }
+    };
+    beforeEach((done) => {
+        let cls = createClass({
+            template: htmlTemplate,
+            directives: [BlockComponent],
+            properties: properties,
+            providers: [
+                helpers.createProviderToValue('SessionService', helpers.mocks.sessionWithCurrentUser({})),
+                helpers.createProviderToValue('AuthService', helpers.mocks.authService),
+                helpers.createProviderToValue('$state', state),
+                helpers.createProviderToValue('TranslatorService', translatorService),
+                helpers.createProviderToValue('$uibModal', helpers.mocks.$modal),
+                helpers.createProviderToValue('BlockService', blockService),
+                helpers.createProviderToValue('NotificationService', helpers.mocks.notificationService)
+            ]
+        });
+        helper = new ComponentTestHelper<BlockComponent>(cls, done);
+    });
+
+    let translatorService = jasmine.createSpyObj("translatorService", ["currentLanguage"]);
+    let blockService = jasmine.createSpyObj("blockService", ["update"]);
+    let state = jasmine.createSpyObj("state", ["current"]);
+    state.current = { name: "" };
+
+    it("set isHomepage as false by default", () => {
+        expect(helper.component.isHomepage).toBeFalsy();
+    });
+
+    it("set isHomepage as true when in profile home page", () => {
+        state.current = { name: "main.profile.home" };
+        helper.component.ngOnInit();
+        expect(helper.component.isHomepage).toBeTruthy();
+    });
+
+    it("set isHomepage as true when in profile info page", () => {
+        state.current = { name: "main.profile.info" };
+        helper.component.ngOnInit();
+        expect(helper.component.isHomepage).toBeTruthy();
+    });
+
+    it("set isHomepage as true when in profile page", () => {
+        state.current = { name: "main.profile.page" };
+        state.params = { page: "/page" };
+        (<noosfero.Profile>helper.component.owner).homepage = '/page';
+        helper.component.ngOnInit();
+        expect(helper.component.isHomepage).toBeTruthy();
+    });
+
+    it("set isHomepage as true when in environment home page", () => {
+        state.current = { name: "main.environment.home" };
+        helper.component.owner = <noosfero.Environment>{};
+        helper.component.ngOnInit();
+        expect(helper.component.isHomepage).toBeTruthy();
+    });
+
+    it("return true in canDisplay when no display option is setted", () => {
+        helper.component.block = <any>{};
+        expect(helper.component.canDisplay()).toEqual(true);
+    });
+
+    it("return false in canDisplay for an invisible block", () => {
+        helper.component.block = <any>{ settings: { display: "never" } };
+        expect(helper.component.canDisplay()).toEqual(false);
+    });
+
+    it("return false in canDisplay with except_home_page in homepage", () => {
+        helper.component.block = <any>{ settings: { display_user: "except_home_page" } };
+        expect(helper.component.canDisplay()).toEqual(false);
+    });
+
+    it("return false in canDisplay with home_page_only outside homepage", () => {
+        helper.component.block = <any>{ settings: { display_user: "home_page_only" } };
+        expect(helper.component.canDisplay()).toEqual(false);
+    });
+
+    it("return true in canDisplay when display_user is all for logged user", () => {
+        helper.component.block = <any>{ settings: { display_user: "all" } };
+        expect(helper.component.canDisplay()).toEqual(true);
+    });
+
+    it("return true in canDisplay when display_user is all for not logged user", () => {
+        helper.component.currentUser = null;
+        helper.component.block = <any>{ settings: { display_user: "all" } };
+        expect(helper.component.canDisplay()).toEqual(true);
+    });
+
+    it("return false in canDisplay when display_user is logged for not logged user", () => {
+        helper.component.currentUser = null;
+        helper.component.block = <any>{ settings: { display_user: "logged" } };
+        expect(helper.component.canDisplay()).toEqual(false);
+    });
+
+    it("return false in canDisplay when display_user is not_logged for logged user", () => {
+        helper.component.block = <any>{ settings: { display_user: "not_logged" } };
+        expect(helper.component.canDisplay()).toEqual(false);
+    });
+
+    it("return false in canDisplay when current language is not equal to language in block settings", () => {
+        helper.component['translatorService'].currentLanguage = jasmine.createSpy("currentLanguage").and.returnValue("pt");
+        helper.component.block = <any>{ settings: { language: "en" } };
+        expect(helper.component.canDisplay()).toEqual(false);
+    });
+
+    it("return false in canDisplay when hide is true", () => {
+        helper.component.block = <any>{ id: 1, hide: true };
+        expect(helper.component.canDisplay()).toEqual(false);
+    });
+
+    it("return true in canDisplay when hide is not true", () => {
+        helper.component.block = <any>{ id: 1, hide: false };
+        expect(helper.component.canDisplay()).toEqual(true);
+    });
+
+});
--- src/app/layout/blocks/block.component.ts
+++ src/app/layout/blocks/block.component.ts
@@ -1,25 +1,58 @@
 import { Input, Component, Inject } from 'ng-forward';
 import { BlockEditionComponent } from './block-edition/block-edition.component';
 import { BlockService } from '../../../lib/ng-noosfero-api/http/block.service';
-import {NotificationService} from '../../shared/services/notification.service';
+import { NotificationService } from '../../shared/services/notification.service';
+import { AuthService, SessionService, AuthEvents } from "../../login";
+import { TranslatorService } from "../../shared/services/translator.service";
 
 @Component({
     selector: 'noosfero-block',
     templateUrl: 'app/layout/blocks/block.html',
     directives: [BlockEditionComponent]
 })
-@Inject("$uibModal", "$scope", BlockService, NotificationService)
+@Inject("$uibModal", "$scope", "$state", "$rootScope", BlockService, NotificationService, AuthService, SessionService, TranslatorService)
 export class BlockComponent {
 
-    @Input() block: any;
-    @Input() owner: any;
+    @Input() block: noosfero.Block;
+    @Input() owner: noosfero.Profile | noosfero.Environment;
 
     private modalInstance: any = null;
     originalBlock: noosfero.Block;
 
-    constructor(private $uibModal: any, private $scope: ng.IScope, private blockService: BlockService, private notificationService: NotificationService) { }
+    currentUser: noosfero.User;
+    isHomepage = true;
+    editionMode = false;
+
+    constructor(private $uibModal: any,
+        private $scope: ng.IScope,
+        private $state: ng.ui.IStateService,
+        private $rootScope: ng.IRootScopeService,
+        private blockService: BlockService,
+        private notificationService: NotificationService,
+        private authService: AuthService,
+        private session: SessionService,
+        private translatorService: TranslatorService) {
+
+        this.currentUser = this.session.currentUser();
+        this.authService.subscribe(AuthEvents[AuthEvents.loginSuccess], () => {
+            this.currentUser = this.session.currentUser();
+            this.verifyHomepage();
+        });
+        this.authService.subscribe(AuthEvents[AuthEvents.logoutSuccess], () => {
+            this.currentUser = this.session.currentUser();
+            this.verifyHomepage();
+        });
+        this.$rootScope.$on("$stateChangeSuccess", (event: ng.IAngularEvent, toState: ng.ui.IState) => {
+            this.verifyHomepage();
+        });
+    }
+
+    ngOnInit() {
+        this.verifyHomepage();
+    }
 
     openEdit() {
+        this.editionMode = true;
         if (!this.originalBlock) this.originalBlock = JSON.parse(JSON.stringify(this.block)); // deep copy of block data
         this.modalInstance = this.$uibModal.open({
             templateUrl: 'app/layout/blocks/block-edition/block-edition.html',
@@ -32,7 +65,8 @@ export class BlockComponent {
     }
 
     save() {
-        this.blockService.update(this.attributesToUpdate(this.block)).then(() => {
+        this.editionMode = false;
+        this.blockService.update(this.attributesToUpdate()).then(() => {
             this.closeEdit();
             this.notificationService.success({ title: "block.edition.success.title", message: "block.edition.success.message" });
         });
@@ -43,18 +77,57 @@ export class BlockComponent {
     }
 
     cancel() {
+        this.editionMode = false;
         this.block = this.originalBlock;
         this.closeEdit();
     }
 
-    protected attributesToUpdate(block: noosfero.Block) {
+    canDisplay() {
+        return this.visible() && this.displayToUser() &&
+            this.displayOnLanguage(this.translatorService.currentLanguage()) &&
+            !this.block.hide;
+    }
+
+    protected visible() {
+        let display = this.block.settings ? (<any>this.block.settings)['display'] : null;
+        return !display || ((this.isHomepage ? display !== "except_home_page" : display !== "home_page_only") && display !== "never");
+    }
+
+    protected displayToUser() {
+        let displayUser = this.block.settings ? (<any>this.block.settings)['display_user'] : null;
+        return !displayUser || displayUser === "all" ||
+            (this.currentUser ? displayUser === "logged" : displayUser === "not_logged");
+    }
+
+    protected displayOnLanguage(language: string) {
+        let displayLanguage = this.block.settings ? (<any>this.block.settings)['language'] : null;
+        return !displayLanguage || displayLanguage === "all" ||
+            language === displayLanguage;
+    }
+
+    protected attributesToUpdate() {
         return <any>{
             id: this.block.id,
-            display: this.block.settings.display,
+            display: (<any>this.block.settings).display,
             title: this.block.title,
-            display_user: this.block.settings.display_user,
-            language: this.block.settings.language
+            display_user: (<any>this.block.settings).display_user,
+            language: (<any>this.block.settings).language
         };
+    }
+
+    protected verifyHomepage() {
+        if (this.owner && ["Profile", "Community", "Person"].indexOf((<any>this.owner)['type']) >= 0) {
+            let profile = <noosfero.Profile>this.owner;
+            this.isHomepage = this.$state.current.name === "main.profile.home";
+            if (profile.homepage) {
+                this.isHomepage = this.isHomepage ||
+                    (this.$state.current.name === "main.profile.page" && profile.homepage === this.$state.params['page']);
+            } else {
+                this.isHomepage = this.isHomepage || this.$state.current.name === "main.profile.info";
+            }
+        } else {
+            this.isHomepage = this.$state.current.name === "main.environment.home";
+        }
     }
 
     private closeEdit() {
