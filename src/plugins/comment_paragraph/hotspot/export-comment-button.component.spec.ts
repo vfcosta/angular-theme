ba519042afe9eb7dc1b69303dbaa5746538b52ca
Victor Costa <vfcosta@gmail.com>
2016-06-15 10:21:35 -0300

    Display button to export comments only to users with permission to edit article

--- src/plugins/comment_paragraph/hotspot/export-comment-button.component.spec.ts
+++ src/plugins/comment_paragraph/hotspot/export-comment-button.component.spec.ts
@@ -3,15 +3,16 @@ import {ComponentTestHelper, createClass} from '../../../spec/component-test-hel
 import * as helpers from "../../../spec/helpers";
 import {Provider} from 'ng-forward';
 import {ComponentFixture} from 'ng-forward/cjs/testing/test-component-builder';
+import {PermissionDirective} from '../../../app/shared/components/permission/permission.directive';
 
 let htmlTemplate = '<export-comment-button-hotspot [article]="ctrl.article"></export-comment-button-hotspot>';
 
 describe("Components", () => {
     describe("Export Comment Button Hotspot Component", () => {
 
-        let serviceMock = jasmine.createSpyObj("CommentParagraphService", [ "getArticle" ]);
+        let serviceMock = jasmine.createSpyObj("CommentParagraphService", ["getArticle"]);
 
-        let providers = [ new Provider('CommentParagraphService', { useValue: serviceMock }) ]
+        let providers = [new Provider('CommentParagraphService', { useValue: serviceMock })]
             .concat(helpers.provideFilters('translateFilter'));
         let helper: ComponentTestHelper<ExportCommentButtonHotspotComponent>;
 
@@ -20,7 +21,7 @@ describe("Components", () => {
         beforeEach((done) => {
             let cls = createClass({
                 template: htmlTemplate,
-                directives: [ExportCommentButtonHotspotComponent],
+                directives: [ExportCommentButtonHotspotComponent, PermissionDirective],
                 providers: providers,
                 properties: {
                     article: {}
@@ -48,5 +49,10 @@ describe("Components", () => {
             expect(helper.all('.export-comment-button').length).toEqual(0);
         });
 
+        it('not display export comment button when user does not have permission', () => {
+            helper.component.article = <noosfero.Article>{ setting: { comment_paragraph_plugin_activate: true } };
+            helper.detectChanges();
+            expect(helper.find('.export-comment-button').attr('style')).toEqual("display: none; ");
+        });
     });
 });
