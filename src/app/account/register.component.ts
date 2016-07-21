015b68e262d6ab38430665210724cb3fba32ac9a
Michel Felipe <mfelipeof@gmail.com>
2016-07-21 16:54:57 -0300

    Added the modal to show environment terms of use text

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -2,6 +2,8 @@ import { Inject, Input, Component, Output, EventEmitter, provide } from 'ng-forw
 import { RegisterService } from "./../../lib/ng-noosfero-api/http/register.service";
 import { NotificationService } from "./../shared/services/notification.service";
 import { EnvironmentService } from "../../lib/ng-noosfero-api/http/environment.service";
+import { RegisterController } from "./register.controller";
+import { IModalComponent } from "../shared/components/interfaces";
 
 @Component({
     selector: 'noosfero-register',
@@ -11,16 +13,20 @@ import { EnvironmentService } from "../../lib/ng-noosfero-api/http/environment.s
     ]
 })
 
-@Inject("$state", RegisterService, NotificationService, EnvironmentService)
+@Inject('$state', '$uibModal', '$scope', RegisterService, NotificationService, EnvironmentService)
 export class RegisterComponent {
     @Input() account: any;
     environment: noosfero.Environment;
 
+    modalInstance: ng.ui.bootstrap.IModalServiceInstance;
+
     constructor(
         private $state: ng.ui.IStateService,
+        private $uibModal: ng.ui.bootstrap.IModalService,
+        private $scope: ng.IScope,
         public registerService: RegisterService,
         private notificationService: NotificationService,
-        private environmentService: EnvironmentService
+        private environmentService: EnvironmentService,
     ) {
         this.account = {};
         this.environment = environmentService.getCurrentEnvironment();
@@ -43,5 +49,17 @@ export class RegisterComponent {
 
     isInvalid(field: Object): Object {
         return { 'has-error': field['$touched'] && field['$invalid'] };
+    }
+
+    openTerms() {
+
+        this.modalInstance = this.$uibModal.open({
+            templateUrl: 'app/account/register-terms.html',
+            size: 'lg',
+            controller: RegisterController,
+            controllerAs: 'vm',
+            bindToController: true,
+            scope: this.$scope
+        });
     }
 }
---
+++ src/app/account/register.controller.ts
@@ -0,0 +1,17 @@
+import { Input } from "ng-forward";
+import { IModalComponent } from "../shared/components/interfaces";
+
+export class RegisterController {
+
+    static $inject = ["$log", "$stateParams", "$scope"];
+    ctrl: IModalComponent;
+
+    constructor(
+        private $log: ng.ILogService,
+        private $stateParams: any,
+    ) { }
+
+    closeTerms() {
+        this.ctrl.modalInstance.dismiss('ok');
+    }
+}
