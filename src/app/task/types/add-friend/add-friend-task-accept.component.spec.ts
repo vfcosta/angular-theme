4813aa7fb6e86bab1b2323377a5ff5f46a782ec7
Marcelo Júnior <maljunior@gmail.com>
2016-11-24 10:39:15 -0300

    Fix test

--- src/app/task/types/add-friend/add-friend-task-accept.component.spec.ts
+++ src/app/task/types/add-friend/add-friend-task-accept.component.spec.ts
@@ -3,18 +3,19 @@ import * as helpers from "../../../../spec/helpers";
 import { ComponentTestHelper, createClass } from '../../../../spec/component-test-helper';
 import { AddFriendTaskAcceptComponent } from './add-friend-task-accept.component';
 
-const htmlTemplate: string = '<abuse-complaint-task-accept [task]="ctrl.task"></abuse-complaint-task-accept>';
+const htmlTemplate: string = '<add-friend-task-accept [task]="ctrl.task" [confirmation-task]="ctrl.confirmationTask"></add-friend-task-accept>';
 
 describe("Components", () => {
 
     describe("Add Friend Task Accept Component", () => {
 
         let helper: ComponentTestHelper<AddFriendTaskAcceptComponent>;
-        let confirmationTask = {
+        let task = <any>{ target: { id: 5 } };
+        let confirmationTaskData = {
                 group_for_friend: 'group1'
             };
-        let taskService = jasmine.createSpyObj("taskService", ["get"]);
-        taskService.get = jasmine.createSpy("get").and.returnValue(Promise.resolve({ headers: () => { }, data: confirmationTask }));
+        let taskService = jasmine.createSpyObj("taskService", ["put"]);
+        taskService.get = jasmine.createSpy("put").and.returnValue(Promise.resolve({ headers: () => { }, data: confirmationTaskData }));
 
         beforeEach(angular.mock.module("templates"));
 
@@ -25,14 +26,14 @@ describe("Components", () => {
                 providers: [
                     helpers.createProviderToValue("TaskService", taskService)
                 ].concat(helpers.provideFilters("translateFilter")),
-                properties: { task: confirmationTask }
+                properties: { task: task, confirmationTask: confirmationTaskData }
             });
             helper = new ComponentTestHelper<AddFriendTaskAcceptComponent>(cls, done);
         });
 
         it("should the add friend have a group named", () => {
-            let reason = 'group1';
-            expect(helper.component.confirmationTask.group_for_friend).toEqual(reason);
+            let group = 'group1';
+            expect(helper.component.confirmationTask.group_for_friend).toEqual(group);
         });
     });
 
