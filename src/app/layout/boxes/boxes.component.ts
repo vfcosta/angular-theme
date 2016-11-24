13b04b5654e6ec84a11c45c6ba75f2af5ca94429
Victor Costa <vfcosta@gmail.com>
2016-11-24 19:39:18 -0300

    Use layout template to define box position

--- src/app/layout/boxes/boxes.component.ts
+++ src/app/layout/boxes/boxes.component.ts
@@ -1,6 +1,6 @@
-import {Input, Component} from 'ng-forward';
-import {DisplayBoxes} from "./display-boxes.filter";
-import {SetBoxLayout} from "./set-box-layout.filter";
+import { Input, Component } from 'ng-forward';
+import { DisplayBoxes } from "./display-boxes.filter";
+import { SetBoxLayout } from "./set-box-layout.filter";
 
 @Component({
     selector: "noosfero-boxes",
@@ -13,4 +13,29 @@ export class BoxesComponent {
     @Input() owner: noosfero.Profile | noosfero.Environment;
     @Input() layout: string;
 
+    layouts = {
+        "topleft": [{ size: 12 }, { size: 3 }, { size: 9 }],
+        "leftbar": [{ size: 3 }, { size: 9 }],
+        "default": [{ size: 3 }, { size: 6 }, { size: 3 }],
+        "lefttopright": [{ size: 3 }, { size: 3, subcolumns: [{ size: 9 }, { size: 3 }] }],
+        "2leftbars": [{ size: 3 }, { size: 3 }, { size: 6 }],
+        "rightbar": [{ size: 9 }, { size: 3 }],
+        "leftbottom": [{ size: 3 }, { size: 9 }, { size: 12 }],
+        "nosidebars": [{ size: 12 }]
+    }
+
+    getLayout() {
+        return this.layouts[this.layout];
+    }
+
+    getBox(index: number) {
+        let layout = this.getLayout();
+        let mainColumn = 1;
+        if (index === mainColumn) {// layout[index].size >= 6) {
+            return this.boxes[0];
+        } else if (index < mainColumn) {
+            return this.boxes[index + 1];
+        }
+        return this.boxes[index];
+    }
 }
