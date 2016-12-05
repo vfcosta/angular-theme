b7afc8eee94959607fc75f94669a450e1c6afb12
Victor Costa <vfcosta@gmail.com>
2016-12-05 17:39:36 -0300

    Improve resize tool of bootstrap resizable directive

--- src/app/shared/components/bootstrap-resizable/bootstrap-resizable.directive.ts
+++ src/app/shared/components/bootstrap-resizable/bootstrap-resizable.directive.ts
@@ -23,10 +23,23 @@ export class BootstrapResizableDirective {
         private $element: ng.IAugmentedJQuery,
         private $window: ng.IWindowService) {
 
-        $element[0].addEventListener('mousedown', (e: MouseEvent) => this.down(e), false);
-        $element[0].addEventListener('touchstart', (e: MouseEvent) => this.down(e), false);
+        this.$element.append('<div class="bootstrap-resize-tool"></div>');
+        $element[0].querySelector('.bootstrap-resize-tool').addEventListener('mousedown', (e: MouseEvent) => this.down(e), false);
+        $element[0].querySelector('.bootstrap-resize-tool').addEventListener('touchstart', (e: MouseEvent) => this.down(e), false);
 
         this.style = $window.getComputedStyle($element[0], null);
+
+        $scope.$watch(() => { return this.isEnabled(); }, () => {
+            let resizeTool = this.$element[0].querySelector('.bootstrap-resize-tool');
+            resizeTool.style['height'] = this.$element[0].clientHeight - 32 + "px";
+            if (this.isEnabled()) {
+                this.$element.addClass('boostrap-resizable');
+                this.$element.removeClass('boostrap-resizable-disabled');
+            } else {
+                this.$element.removeClass('boostrap-resizable');
+                this.$element.addClass('boostrap-resizable-disabled');
+            }
+        });
     }
 
     isEnabled() {
@@ -81,7 +94,6 @@ export class BootstrapResizableDirective {
         let currentColumns = this.getCurrentColumns();
         let newColumns = Math.round((currentColumns * (this.w - offset)) / this.$element[0].clientWidth);
         newColumns = Math.max(1, Math.min(12, newColumns));
-        // console.log("RIGHT", newColumns, this.$element[0].clientWidth, this.w - offset);
         this.replaceColumnClass(currentColumns, newColumns);
     }
 
