4167801a77147e1cf0b487d1191637cf9ecfae81
Leandro Nunes dos Santos <leandronunes@gmail.com>
2016-12-12 09:33:44 -0300

    remove border from title and make some tests

--- src/app/layout/blocks/block.component.spec.ts
+++ src/app/layout/blocks/block.component.spec.ts
@@ -150,4 +150,16 @@ describe("Boxes Component", () => {
     it("set block columns according to visualization settings", () => {
         expect(helper.all(".noosfero-block.col-md-7").length).toEqual(1);
     });
+
+    it("display block title if it's in design mode", () => {
+        helper.component.block = <any>{ id: 1, title: '' };
+        helper.component.designMode = true;
+        expect(helper.all(".panel-title").length).toEqual(1);
+    });
+
+    it("display block title if the block has title", () => {
+        helper.component.block = <any>{ id: 1, title: 'some title' };
+        expect(helper.all(".panel-title").length).toEqual(1);
+    });
+
 });
