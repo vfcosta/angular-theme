d8ffde50a33610d3333c461e6ffb0693604409d3
Leonardo Soares <leors75@gmail.com>
2016-12-13 15:49:09 -0300

    Added ability to edit section block

--- src/plugins/section/blocks/section-block/section-block.component.ts
+++ src/plugins/section/blocks/section-block/section-block.component.ts
@@ -1,23 +1,34 @@
-import {Component, Inject, Input} from "ng-forward";
+import { Component, Inject, Input } from "ng-forward";
+import { DesignModeService } from "../../../../app/admin/layout-edit/designMode.service";
 
 @Component({
     selector: "noosfero-section-block-plugin-section-block",
     templateUrl: "plugins/section/blocks/section-block/section-block.html"
 })
+@Inject( "$scope", DesignModeService)
 export class SectionBlockComponent {
 
     @Input() block: any;
     @Input() owner: any;
 
-    name: string;
-    description: string;
     font_color: string;
     background_color: string;
+    designMode = false;
+
+
+    constructor(private $scope: ng.IScope, 
+    private designModeService: DesignModeService) {
+
+        this.designModeService.onToggle.subscribe((designModeOn: boolean) => {
+            this.designMode = designModeOn;
+            this.$scope.$apply();
+        });
+
+        this.designMode = this.designModeService.isInDesignMode()
+    }
 
     ngOnInit() {
         if (this.block && this.block.settings) {
-            this.name = this.block.settings.name.trim();
-            this.description = this.block.settings.description.trim();
             this.font_color = this.block.settings.font_color.trim();
             this.background_color = this.block.settings.background_color.trim();
         }
@@ -34,18 +45,18 @@ export class SectionBlockComponent {
             css_property = 'color:';
         }
         else {
-          if (color_type === 'background') {
-              css_property = 'background-color:';
-          }
-          else {
-              return css_property;
-          }
+            if (color_type === 'background') {
+                css_property = 'background-color:';
+            }
+            else {
+                return css_property;
+            }
         }
 
         let css_style = '';
 
         if (color_value !== '') {
-          css_style = css_property + ' #' + color_value + ';';
+            css_style = css_property + ' #' + color_value + ';';
         }
         return css_style;
     }
