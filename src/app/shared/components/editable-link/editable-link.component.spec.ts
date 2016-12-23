d36775078c24cfdc12d4fd7651b322a29098f824
Victor Costa <vfcosta@gmail.com>
2016-12-23 09:25:41 -0300

    Accept name and address as separated inputs in editable link

--- src/app/shared/components/editable-link/editable-link.component.spec.ts
+++ src/app/shared/components/editable-link/editable-link.component.spec.ts
@@ -4,7 +4,7 @@ import { EditableLinkComponent } from './editable-link.component';
 import * as helpers from "../../../../spec/helpers";
 import { ComponentTestHelper, createClass } from '../../../../spec/component-test-helper';
 
-const htmlTemplate: string = '<noosfero-editable-link [link]="ctrl.link" [desig-mode]="ctrl.designMode" [opened]="ctrl.opened"></noosfero-editable-link>';
+const htmlTemplate: string = '<noosfero-editable-link [name]="ctrl.name" [address]="ctrl.address" [desig-mode]="ctrl.designMode" [opened]="ctrl.opened"></noosfero-editable-link>';
 
 describe("Components", () => {
 
@@ -19,7 +19,8 @@ describe("Components", () => {
                 template: htmlTemplate,
                 directives: [EditableLinkComponent],
                 properties: {
-                    link: { name: 'link', address: 'address' },
+                    name: 'link',
+                    address: 'address',
                     designMode: true,
                     popupOpen: false
                 }
@@ -34,7 +35,8 @@ describe("Components", () => {
         it("copy link content when save", () => {
             helper.component.modifiedLink = { name: "modified name", address: "modified address" };
             helper.component.save();
-            expect(helper.component.link).toEqual({ name: 'modified name', address: 'modified address' });
+            expect(helper.component.name).toEqual('modified name');
+            expect(helper.component.address).toEqual('modified address');
         });
 
         it("set popupOpen to false when cancel", () => {
