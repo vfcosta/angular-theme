38de8c40c0d74b4cc548f87c23bd658825417b8a
Victor Costa <vfcosta@gmail.com>
2017-01-16 12:12:46 -0300

    Migrate to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,16 @@
 var argv = require("yargs").argv;
 var path = require("path");
 var glob = require("glob");
+var testProcess = null;
+var child_process = require("child_process");
+var count = 0;
+var stdinPatched = false;
 
 var WebpackOnBuildPlugin = require('on-build-webpack');
 
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
 var extension = ".js";
-if (argv.production) {
-    extension = ".min.js"
-}
-
 var testFiles = glob.sync("./src/**/**/*.[sS]pec.ts");
 
 var uglifyLoaderConfig = {
@@ -31,92 +31,92 @@ var entries = {
     'vendor.bundle': ['core-js', 'reflect-metadata', 'ng-forward', 'ng-forward/cjs/testing/test-component-builder']
 };
 
-var webpackConfig = {
-    entry: entries,
-
-    plugins: [new CommonsChunkPlugin("commons", "commons.js")],
-
-    output: {
-        path: path.join(__dirname, "src"),
-        filename: "[name]" + extension,
-    },
-
-    resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
-    },
-    // Source maps support (or 'inline-source-map' also works)
-    devtool: 'source-map',
-
-    module: {
-        loaders: [{
-            test: /\.css$/,
-            loader: "style!css"
-        }, {
-            test: /\.scss$/,
-            loaders: ["style", "css?sourceMap", "sass?sourceMap"]
-        }, {
-            test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-            loader: 'url-loader?limit=100000'
-        }, {
-            test: /\.tsx?$/,
-            loader: 'ts-loader'
-        }, {
-            test: /\.ts?$/,
-            loader: "tslint"
-        }]
+module.exports = function(env) {
+    if (env && env.production) {
+        extension = ".min.js"
     }
+
+    var config = {
+        entry: entries,
+
+        plugins: [new CommonsChunkPlugin({name: "commons", filename: "commons.js"})],
+
+        output: {
+            path: path.join(__dirname, "src"),
+            filename: "[name]" + extension,
+        },
+
+        resolve: {
+            // Add `.ts` and `.tsx` as a resolvable extension.
+            extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+        },
+        // Source maps support (or 'inline-source-map' also works)
+        devtool: 'source-map',
+
+        module: {
+            rules: [{
+                test: /\.css$/,
+                loader: "style!css"
+            }, {
+                test:        /\.scss$/,
+                use: [
+                    {loader: "style"}, {loader: "css?sourceMap"}, {loader: "sass?sourceMap"}
+                ]
+            }, {
+                test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+                loader: 'url-loader?limit=100000'
+            }, {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }, {
+                test: /\.ts?$/,
+                loader: "tslint-loader"
+            }]
+        }
+    };
+
+
+    // if webpack executed with --test argument, this code bellow will add a post-compilation
+    // code which will run the Tests + Coverage (npm run coverage)
+    if (env && env.test) {
+        function spawnChildProcessTest() {
+            if (!testProcess) {
+                testProcess = child_process.spawn("npm", ["run", "coverage"], {
+                    stdio: 'inherit'
+                });
+                testProcess.on('exit', function() {
+                    testProcess = null
+                });
+            }
+        }
+        // configure the webPackOnBuildPlugin with our post-compilation function as argument
+        var onBuildPluginConfigured = new WebpackOnBuildPlugin(function(stats) {
+
+            // here we are patching the stdin to allow trigger tests when pressing 'Enter'
+            // on terminal
+            if (!env.singleRun && !stdinPatched) {
+                process.stdin.on('data', function(info) {
+                    if (info == '\n') {
+                        spawnChildProcessTest();
+                    }
+                });
+                stdinPatched = true;
+            }
+
+            // webpack in watch mode call this twice for just one compilation    
+            // so, here we are checking if the process is still running before trigger another test execution
+            if (testProcess == null) {
+                console.log("Starting tests execution...");
+                spawnChildProcessTest();
+            } else {
+                console.log("Test still running... Sorry webpack!! :)");
+            }
+
+        });
+        config.plugins.push(onBuildPluginConfigured);
+    }
+    if (env && env.production) {
+        config.module.loaders.push(uglifyLoaderConfig);
+    }
+    return config;
 };
-
-if (argv.production) {
-    webpackConfig.module.loaders.push(uglifyLoaderConfig);
-}
-
-
-// if webpack executed with --test argument, this code bellow will add a post-compilation
-// code which will run the Tests + Coverage (npm run coverage)
-var testProcess = null;
-var child_process = require("child_process");
-var count = 0;
-var stdinPatched = false;
-
-
-if (argv.test) {
-    function spawnChildProcessTest() {
-        if (!testProcess) {
-            testProcess = child_process.spawn("npm", ["run", "coverage"], {
-                stdio: 'inherit'
-            });
-            testProcess.on('exit', function() {
-                testProcess = null
-            });
-        }
-    }
-    // configure the webPackOnBuildPlugin with our post-compilation function as argument
-    var onBuildPluginConfigured = new WebpackOnBuildPlugin(function(stats) {
-
-        // here we are patching the stdin to allow trigger tests when pressing 'Enter'
-        // on terminal
-        if (!argv.singleRun && !stdinPatched) {
-            process.stdin.on('data', function(info) {
-                if (info == '\n') {
-                    spawnChildProcessTest();
-                }
-            });
-            stdinPatched = true;
-        }
-
-        // webpack in watch mode call this twice for just one compilation    
-        // so, here we are checking if the process is still running before trigger another test execution
-        if (testProcess == null) {
-            console.log("Starting tests execution...");
-            spawnChildProcessTest();
-        } else {
-            console.log("Test still running... Sorry webpack!! :)");
-        }
-
-    });
-    webpackConfig.plugins.push(onBuildPluginConfigured);
-}
-
-module.exports = webpackConfig;
