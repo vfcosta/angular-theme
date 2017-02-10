da26d76bfb4c9a10a417fb1c1cea1c5d698423fa
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-02-10 18:02:27 -0300

    load profile in about page closes #273

--- src/app/profile/data/profile-data.component.spec.ts
+++ src/app/profile/data/profile-data.component.spec.ts
@@ -1,62 +1,44 @@
 import * as helpers from "./../../../spec/helpers";
-
 import {Injectable, Provider, provide} from "ng-forward";
 import {providers} from 'ng-forward/cjs/testing/providers';
-
-import {ComponentFixture} from 'ng-forward/cjs/testing/test-component-builder';
-
 import {ProfileDataComponent} from "./profile-data.component";
 import {TranslateProfile} from '../../shared/pipes/translate-profile.filter';
+import {ComponentTestHelper, createClass} from '../../../spec/component-test-helper';
 
-let templateHtml = '<profile-data [profile]="ctrl.profile"></profile-data>';
+let htmlTemplate = '<profile-data></profile-data>';
 
 describe('Profile data component', () => {
 
-    let profileMock = <noosfero.Profile>{
-        id: 1,
-        name: 'Profile Test',
-        identifier: 'profile-test',
-        type: 'Person'
+    let profileService: any;
+    let profile = {
+        id: 1, identifier: "profile-test", additional_data: { 'Address': 'Street A, Number 102' }
     };
 
-    beforeEach(() => {
+    let helper: ComponentTestHelper<ProfileDataComponent>;
+    beforeEach(angular.mock.module("templates"));
 
-        angular.mock.module("templates");
-        angular.mock.module("angularMoment");
+    beforeEach((done) => {
+        profileService = jasmine.createSpyObj("profileService", ["getCurrentProfile"]);
+        profileService.getCurrentProfile = jasmine.createSpy("getCurrentProfile").and.returnValue(helpers.mocks.promiseResultTemplate(profile));
 
-        providers((provide: any) => {
-            return <any>helpers.provideFilters('TranslateProfile', 'translateFilter');
+
+        let cls = createClass({
+            template: htmlTemplate,
+            directives: [ProfileDataComponent],
+            providers: [
+                helpers.createProviderToValue('ProfileService', profileService)
+            ]
         });
+        helper = new ComponentTestHelper<ProfileDataComponent>(cls, done);
     });
 
-    let buildComponent = (): Promise<ComponentFixture> => {
-        return helpers.quickCreateComponent({
-            directives: [ProfileDataComponent],
-            template: templateHtml,
-            properties: {
-                profile: profileMock
-            }
-        });
-    };
-
-    it('renders profile-data directive', () => {
-        buildComponent().then((fixture: ComponentFixture) => {
-            let profileData: ProfileDataComponent = fixture.debugElement.componentViewChildren[0].componentInstance;
-
-            expect(fixture.debugElement.query('div.table-responsive').length).toEqual(1);
-            expect(fixture.debugElement.query('span.label-info').length).toEqual(1);
-        });
+    it("renders profile-data directive", () => {
+        expect(helper.all("div.table-responsive").length).toEqual(1);
+        expect(helper.all("span.label-info").length).toEqual(1);
     });
 
     it('renders profile-data directive with custom fields', () => {
-        profileMock.additional_data = {
-            'Address': 'Street A, Number 102'
-        };
-
-        buildComponent().then((fixture: ComponentFixture) => {
-            let profileData: ProfileDataComponent = fixture.debugElement.componentViewChildren[0].componentInstance;
-            profileData.profile = profileMock;
-            expect(fixture.debugElement.query('div.profile-custom-fields').length).toEqual(1);
-        });
+        expect(helper.all("div.profile-custom-fields").length).toEqual(1);
     });
+
 });
