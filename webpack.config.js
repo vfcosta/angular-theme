bb99251311b59c8e8c2037e92f9739449d9a4f83
Victor Costa <vfcosta@gmail.com>
2017-03-03 14:13:09 -0300

    Fix test process

--- webpack.config.js
+++ webpack.config.js
@@ -23,12 +23,11 @@ var uglifyLoaderConfig = {
     loader: "uglify"
 };
 
-var testingFiles = glob.sync("./src/app/**/**/*.[sS]pec.ts");
-
+var testingFiles = glob.sync("./src/app/**/*.spec.ts");
 
 var entries = {
     noosfero: './src/app/boot.ts',
-    'noosfero-test': './src/app/index.ts',
+    'noosfero-test': ['./src/app/index.ts', './karma.entry.js'],
     'noosfero-specs': testFiles, // './src/specs.ts',
     'vendor.bundle': ['core-js', 'reflect-metadata', 'ng-forward', 'ng2-img-cropper',
       'ng-forward/cjs/testing/test-component-builder', 'zone.js', 'moment',
@@ -44,7 +43,11 @@ module.exports = function(env) {
         entry: entries,
 
         plugins: [
-            new CommonsChunkPlugin({name: "commons", filename: "commons.js"}),
+            new CommonsChunkPlugin({name: "commons", filename: "commons.js",
+                minChunks: function(module, count) {
+                    return module.resource && (/node_modules/).test(module.resource) && count >= 2;
+                }
+            }),
             new ContextReplacementPlugin(
                 /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
                 path.resolve(__dirname, 'doesnotexist/')
