5d6dedd0186ced9455e08c1f35d28538c25476e2
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-03-03 14:05:00 -0300

    fix profile members list

--- src/app/profile/lists/list-profiles.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component, Input} from '@angular/core';
-
-@Component({
-    selector: 'profile-list',
-    template: require("app/profile/lists/list-profiles.component.html")
-})
-export class ListProfilesComponent {
-    @Input() profiles: noosfero.Profile[];
-}
