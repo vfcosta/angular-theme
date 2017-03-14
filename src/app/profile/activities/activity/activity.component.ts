508ebae0152d6955150f823df40fb779e5171b4d
Carlos Purificacao <carloseugenio@gmail.com>
2017-03-14 11:04:02 -0300

    Tests working

--- src/app/profile/activities/activity/activity.component.ts
+++ src/app/profile/activities/activity/activity.component.ts
@@ -1,17 +1,18 @@
 import {Component, Input, Inject} from "ng-forward";
 import { EnvironmentService } from "../../../../lib/ng-noosfero-api/http/environment.service";
 import { ActivityHeaderComponent } from "./activityheader.component";
-import { TranslatorService } from "../../../shared/services/translator.service";
 
 @Component({
     selector: "noosfero-activity",
     templateUrl: 'app/profile/activities/activity/activity.html',
     directives: [ActivityHeaderComponent]
 })
-@Inject(EnvironmentService, TranslatorService)
+@Inject(EnvironmentService)
 export class ActivityComponent {
 
     @Input() activity: noosfero.Activity;
+
+    profiles = [];
 
     images = {
         'new_friendship': 'friend_profile_custom_icon',
@@ -31,35 +32,32 @@ export class ActivityComponent {
 
     environment: noosfero.Environment;
 
-    constructor(private environmentService: EnvironmentService, private translatorService: TranslatorService) {
+    constructor(private environmentService: EnvironmentService) {
         environmentService.getCurrentEnvironment().then((environment: noosfero.Environment) => {
             this.environment = environment;
         });
-
     }
 
+    /**
+    * Loads the profiles array associated with the current activity. Doing this in the view
+    * causes errors on Angular
+    */
     ngOnInit() {
-        console.log("onInit.Activity => ", this.activity);        
+        //console.log("onInit.Activity => ", this.activity);
+        if (this.activity.verb == 'new_follower' || this.activity.verb == 'new_friendship') {
+            this.profiles_list(this.activity.verb);
+        }
     }
 
     getActivityTemplate() {
-        console.log("Activity: ", this.activity);
         return 'app/profile/activities/activity/' + this.activity.verb + '.html';
     }
 
-    description() {
-        console.log("Verb: ", this.activity.verb);
-        console.log("Translating....");
-        let key = "activities." + this.activity.verb + ".description";
-        let t1 = this.translatorService.translate(key);
-        console.log("Translation for [" + key + "] => ", t1);
-        return key;
-    }
-
+    /**
+    * Returns the number of profiles associated with the current activity
+    */
     profiles() {
         let profiles_attribute = this.profile_attribute();
-        console.log("Profiles_attribute: ", profiles_attribute);
-        console.log("Activity params: ", this.activity.params);
         if (profiles_attribute && this.activity.params) {
             let profiles_length = this.activity.params[profiles_attribute].length;
             console.log("Profiles length: ", profiles_length);
@@ -68,20 +66,44 @@ export class ActivityComponent {
         return 0;
     }
 
+    /**
+    * Create a list of Profiles based on activity name, image url and identifier
+    */
+    private profiles_list(type: string) {
+        let profiles_attribute = this.profile_attribute();
+        for (var i = this.activity.params[profiles_attribute].length - 1; i >= 0; i--) {
+            let profile = { 
+                name: this.name(i), 
+                identifier: this.url(i),
+                image: { url: this.icon(i) }
+            };
+            this.profiles.push(profile);
+        }
+    }
+
     profile_attribute() {
         return this.profiles_attributes[this.activity.verb];
     }
 
-    profile_is_array() {
-        if (this.activity.params) {
-            return Array.isArray(this.activity.params[this.profile_attribute()]);
+    icon(index: any) {
+        let image_attribute = this.images[this.activity.verb];
+        if (image_attribute) {
+            return this.activity.params[image_attribute][index];
         }
-        return false;
+        return '';
+    }
+
+    name(index: any) {
+        let name_attribute = this.profiles_attributes[this.activity.verb];
+        if (name_attribute) {
+            return this.activity.params[name_attribute][index];
+        }
+        return '';
     }
 
     url(index: any) {
         let url_attribute = this.urls_attributes[this.activity.verb];
-        console.log("Url for index[" + index + "]=>", url_attribute);
+        //console.log("Url for index[" + index + "]=>", url_attribute);
         if (url_attribute) {
             let profile_url = this.activity.params[url_attribute][index].profile;
             return profile_url;
