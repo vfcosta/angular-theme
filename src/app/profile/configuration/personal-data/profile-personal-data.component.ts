429604bc5c2f356d8978ae1ce16e25f2e22c86f6
Josafá Souza Jr <josafa.souza@serpro.gov.br>
2017-03-28 15:38:59 -0300

    Profile configuration menu and personal data edition

--- src/app/profile/configuration/personal-data/profile-personal-data.component.ts
+++ src/app/profile/configuration/personal-data/profile-personal-data.component.ts
@@ -1,7 +1,7 @@
+import { TranslatorService } from './../../../shared/services/translator.service';
 import { NotificationService } from './../../../shared/services/notification.service';
-import { EventEmitter } from 'ng-forward';
 import { ProfileService } from './../../../../lib/ng-noosfero-api/http/profile.service';
-import { Component, Input, Inject, Output, ViewChild } from '@angular/core';
+import { Component, Input, Inject, Output, ViewChild, EventEmitter } from '@angular/core';
 
 /**
  * @ngdoc controller
@@ -26,9 +26,10 @@ export class ProfilePersonalDataComponent {
 
     errors: any;
 
-    constructor(@Inject("profileService") private profileService: ProfileService,
+    constructor( @Inject("profileService") private profileService: ProfileService,
         @Inject("notificationService") private notificationService: NotificationService,
-        @Inject("$state") private $state: ng.ui.IStateService) { }
+        @Inject("$state") private $state: ng.ui.IStateService,
+        @Inject("translatorService") private translatorService: TranslatorService) { }
 
     ngOnInit() {
         this.cloneProfile();
@@ -40,31 +41,44 @@ export class ProfilePersonalDataComponent {
             let identifierChanged = this.profile.identifier !== this.updatedProfile.identifier;
             this.profile = Object.assign(this.profile, this.updatedProfile);
             this.notificationService.success({ title: "profile.edition.success.title", message: "profile.edition.success.message" });
-            // this.finished.emit(this.profile);
+            this.finished.emit(this.profile);
             if (identifierChanged) {
                 // go to the state with the new identifier url
                 this.$state.go(this.$state.current, { profile: this.profile.identifier }, { reload: true });
             }
         }).catch((response) => {
-            this.errors = response.data.message;
-            this.identifierErrors.setErrors(this.errors['identifier']);
+            this.errors = response.data.error;
+            console.log(this.errors);
+            this.notificationService.error({ title: "profile.edition.error.title", message: "profile.edition.error.message" });
+            // this.identifierErrors.setErrors(this.errors['identifier']);
         });
     }
 
     cancel() {
-        // this.finished.emit(this.profile);
+        this.finished.emit(this.profile);
     }
 
     cloneProfile() {
-        this.updatedProfile = this.profile;
+        this.updatedProfile = <noosfero.Profile>['id', 'name', 'email', 'additional_data'].reduce((object, key) => {
+            object[key] = this.profile[key]; return object;
+        }, {});
         this.customFieldsDict = this.updatedProfile.additional_data;
     }
 
     keys(): Array<string> {
         return Object.keys(this.customFieldsDict);
     }
+
+    translateLabel(label) {
+        let translation = this.translatorService.translate('profile.edition.' + label);
+        return translation.indexOf('profile.edition.') >= 0 ? label : translation;
+    }
+
+    fieldType(label) {
+        return label.indexOf('date') >= 0 ? 'date' : 'text';
+    }
 }
 
 interface Dictionary {
-    [ index: string ]: string;
+    [index: string]: string;
 }
