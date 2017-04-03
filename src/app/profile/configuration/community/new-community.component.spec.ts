3f694fbdd111794617653838efb45b765941a59d
Leonardo Soares <leors75@gmail.com>
2017-04-03 14:01:55 -0300

    fixing tests

--- src/app/profile/configuration/community/new-community.component.spec.ts
+++ src/app/profile/configuration/community/new-community.component.spec.ts
@@ -4,8 +4,10 @@ import { By } from '@angular/platform-browser';
 import { async, fakeAsync, tick, TestBed, ComponentFixture } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import * as helpers from "../../../../spec/helpers";
-import { FormsModule, NG_VALIDATORS, AbstractControl,
-         NgForm, FormControl } from '@angular/forms';
+import {
+    FormsModule, NG_VALIDATORS, AbstractControl,
+    NgForm, FormControl
+} from '@angular/forms';
 
 describe("Components", () => {
 
@@ -13,7 +15,8 @@ describe("Components", () => {
         let fixture: ComponentFixture<NewCommunityComponent>;
         let component: NewCommunityComponent;
         let communityService = jasmine.createSpyObj("communityService", ["createNewCommunity"]);
-        communityService.createNewCommunity = jasmine.createSpy("createNewCommunity").and.returnValue(Promise.resolve({ }));
+        let state = jasmine.createSpyObj("$state", ["go"]);
+        communityService.createNewCommunity = jasmine.createSpy("createNewCommunity").and.returnValue(Promise.resolve({}));
 
         beforeEach(async(() => {
             TestBed.configureTestingModule({
@@ -22,13 +25,15 @@ describe("Components", () => {
                 providers: [
                     { provide: "communityService", useValue: communityService },
                     { provide: "notificationService", useValue: helpers.mocks.notificationService },
+                    { provide: "$state", useValue: state },
                     { provide: "translatorService", useValue: helpers.mocks.translatorService },
                 ],
                 schemas: [CUSTOM_ELEMENTS_SCHEMA]
             }).compileComponents().then(() => {
                 fixture = TestBed.createComponent(NewCommunityComponent);
                 component = fixture.componentInstance;
-                component.community = <noosfero.Community>{ name: 'community test', closed: true};
+                component.community = <noosfero.Community>{ name: 'community test', closed: true };
+                component.profile = <noosfero.Profile>{ identifier: '0' };
             });
         }));
 
