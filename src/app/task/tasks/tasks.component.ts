b785ba2e7fbb15f4d66412ae8afa6ac84cab89c8
Josafá Souza Jr <josafa.souza@serpro.gov.br>
2017-04-03 17:07:48 -0300

    Separate friendship task menu from the others

--- src/app/task/tasks/tasks.component.ts
+++ src/app/task/tasks/tasks.component.ts
@@ -1,24 +1,26 @@
-import { Component, Inject, provide } from "ng-forward";
+import { Component, Inject, provide, Input } from "ng-forward";
 import { TaskService } from "../../../lib/ng-noosfero-api/http/task.service";
 
 @Component({
     selector: "tasks",
     templateUrl: "app/task/tasks/tasks.html"
 })
-@Inject(TaskService)
+@Inject(TaskService, '$stateParams')
 export class TasksComponent {
 
     tasks: noosfero.Task[];
     total: number;
     currentPage = 1;
     perPage = 5;
+    taskTypes: string;
 
-    constructor(private TaskService: TaskService) {
+    constructor(private TaskService: TaskService, private $stateParams: ng.ui.IStateParamsService) {
+        this.taskTypes = $stateParams['taskTypes'];
         this.loadPage();
     }
 
     loadPage() {
-        this.TaskService.getAllPending({ page: this.currentPage, per_page: this.perPage }).then((result: noosfero.RestResult<noosfero.Task[]>) => {
+        this.TaskService.getAllPending({content_type: this.taskTypes, page: this.currentPage, per_page: this.perPage }).then((result: noosfero.RestResult<noosfero.Task[]>) => {
             this.total = result.headers('total');
             this.tasks = result.data;
         });
