d5b4e718e1d1e4188a76df963be300d32b8a2e70
Victor Costa <vfcosta@gmail.com>
2017-04-19 18:54:38 -0300

    Add end to end tests for homepage

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,27 +1,12 @@
-'use strict';
-
-var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
-
-// An example configuration file.
 exports.config = {
-  // The address of a running selenium server.
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
-
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
+  framework: 'jasmine',
+  specs: ['e2e/**/*.js'],
+  directConnect: true,
+  baseUrl: 'http://localhost:3001',
+  rootElement: 'body',
+  onPrepare: function() {
+    browser.manage().timeouts().implicitlyWait(25000);
+    browser.ignoreSynchronization = true;
   },
+}
 
-  baseUrl: 'http://localhost:3000',
-
-  // Spec patterns are relative to the current working directory when
-  // protractor is called.
-  specs: [paths.e2e + '/**/*.js'],
-
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
-};
