a830c215faffb975bef5722280a5b1db4cdda4db
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-04-24 17:07:15 -0300

    set styles to section block

--- src/plugins/section_block/blocks/section-block/section-block.component.ts
+++ src/plugins/section_block/blocks/section-block/section-block.component.ts
@@ -12,47 +12,29 @@ export class SectionBlockComponent {
     @Input() designMode: boolean;
 
     @ViewChild("popover") popover;
-    modifiedLink: any;
 
-    font_color: string;
-    background_color: string;
+    modifiedLink: any;
+    fontColor: string;
+    backgroundColor: string;
 
     constructor( @Inject('blockService') private blockService: BlockService) { }
 
     ngOnInit() {
         if (this.block && this.block.settings) {
-            this.font_color = this.block.settings.font_color;
-            this.background_color = this.block.settings.background_color;
+            this.fontColor = this.block.settings.fontColor;
+            this.backgroundColor = this.block.settings.backgroundColor;
         }
         this.copyLink();
     }
 
-    colors() {
-        return (this.color('font', this.font_color) + ' ' + this.color('background', this.background_color));
+    setStyles() {
+        let styles = {
+            'background-color': ('#' + this.backgroundColor || 'inherit'),
+            'color': ('#' + this.fontColor || 'inherit')
+        };
+        return styles;
     }
 
-    color(color_type: string, color_value: string) {
-        let css_property = '';
-
-        if (color_type === 'font') {
-            css_property = 'color:';
-        }
-        else {
-            if (color_type === 'background') {
-                css_property = 'background-color:';
-            }
-            else {
-                return css_property;
-            }
-        }
-
-        let css_style = '';
-
-        if (color_value !== '') {
-            css_style = css_property + ' #' + color_value + ';';
-        }
-        return css_style;
-    }
 
     upload(data: any) {
         this.blockService.uploadImages(this.block, [data]).then((result: noosfero.RestResult<noosfero.Block>) => {
