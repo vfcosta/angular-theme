584baa2f8d42ca317b37be664cd25d9c5fef6311
Marcelo Júnior <maljunior@gmail.com>
2017-06-14 13:20:51 -0300

    fix person tags block not displaying tags

--- src/lib/ng-noosfero-api/http/person.service.ts
+++ src/lib/ng-noosfero-api/http/person.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs/Observable';
 @Inject("Restangular", "$q", "$log", ProfileService)
 export class PersonService extends RestangularService<noosfero.Person> {
 
-    constructor(Restangular: restangular.IService, $q: ng.IQService, $log: ng.ILogService, protected profileService: ProfileService) {
-        super(Restangular, $q, $log);
+    constructor(private restangular: restangular.IService, $q: ng.IQService, $log: ng.ILogService, protected profileService: ProfileService) {
+        super(restangular, $q, $log);
     }
 
     getResourcePath() {
@@ -47,7 +47,7 @@ export class PersonService extends RestangularService<noosfero.Person> {
     }
 
     getTags(profile: noosfero.Profile): ng.IPromise<noosfero.RestResult<any>> {
-        let p = this.getElement(<number>profile.id).customGET('tags');
+        let p = this.restangular.one('profiles', <number>profile.id).customGET('tags');
         let deferred = this.$q.defer<noosfero.RestResult<any>>();
         p.then(this.getHandleSuccessFunction<noosfero.RestResult<any>>(deferred));
         p.catch(this.getHandleErrorFunction<noosfero.RestResult<any>>(deferred));
