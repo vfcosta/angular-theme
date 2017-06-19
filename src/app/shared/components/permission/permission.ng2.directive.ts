28d3f58970dbb2f6a67d5c1255c2801175581958
Josafá Souza Jr <josafa.souza@serpro.gov.br>
2017-06-19 11:29:09 -0300

    Fix bug to permit users that can manage contet to post articles

--- src/app/shared/components/permission/permission.ng2.directive.ts
+++ src/app/shared/components/permission/permission.ng2.directive.ts
@@ -13,11 +13,26 @@ export class PermissionNg2Directive implements OnChanges {
     constructor(private element: ElementRef, private renderer: Renderer) { }
 
     ngOnChanges(changes: any) {
-        let permString = this.permission.toString();
-        if (!this.permission || !_.includes(this.permissionAction, permString)) {
-            this.renderer.setElementStyle(this.element.nativeElement, "display", "none");
+        let render = false;
+        if (this.permissionAction) {
+            let perms = this.permissionAction.split(",");
+            _.forEach(perms, permAction => {
+                _.forEach(this.permission, perm => {
+                    if (perm === permAction) {
+                        render = true;
+                        return false;
+                    }
+                });
+                if (render)
+                    return false;
+            });
+            if (!render) {
+                this.renderer.setElementStyle(this.element.nativeElement, "display", "none");
+            } else {
+                this.renderer.setElementStyle(this.element.nativeElement, "display", "");
+            }
         } else {
-            this.renderer.setElementStyle(this.element.nativeElement, "display", "");
+            this.renderer.setElementStyle(this.element.nativeElement, "display", "none");
         }
     }
 }
