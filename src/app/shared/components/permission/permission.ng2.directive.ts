968a5ff8e7e5ce663f2c3dada8c96289e2bf9855
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-06-20 16:26:54 -0300

    refactoring visibility based on permission

--- src/app/shared/components/permission/permission.ng2.directive.ts
+++ src/app/shared/components/permission/permission.ng2.directive.ts
@@ -13,26 +13,10 @@ export class PermissionNg2Directive implements OnChanges {
     constructor(private element: ElementRef, private renderer: Renderer) { }
 
     ngOnChanges(changes: any) {
-        let render = false;
-        if (this.permissionAction) {
-            let perms = this.permissionAction.split(",");
-            _.forEach(perms, permAction => {
-                _.forEach(this.permission, perm => {
-                    if (perm === permAction) {
-                        render = true;
-                        return false;
-                    }
-                });
-                if (render)
-                    return false;
-            });
-            if (!render) {
-                this.renderer.setElementStyle(this.element.nativeElement, "display", "none");
-            } else {
-                this.renderer.setElementStyle(this.element.nativeElement, "display", "");
-            }
-        } else {
+        if(_.isEmpty(_.intersectionBy(this.permission, this.permissionAction.split(',')))){
             this.renderer.setElementStyle(this.element.nativeElement, "display", "none");
+        } else{
+            this.renderer.setElementStyle(this.element.nativeElement, "display", "");
         }
     }
 }
