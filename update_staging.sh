10d4ff3f766f4f8eafbdcf65fb4a1ccea993aea7
Evandro Junior <evandrojr@gmail.com>
2017-06-23 11:32:20 -0300

    script para atualizar o servidor de homologacao

---
+++ update_staging.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git checkout master
+git pull
+git checkout staging
+git pull
+git merge --strategy-option theirs
+npm run build-all
+git add -f dist
+git commit -m "Version for staging's publication"
+git push
